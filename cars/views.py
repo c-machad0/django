13095297e5c8242c4c69e81571ceddc6b28a54e8
Christian Machado <realchris.machado@gmail.com>
2025-10-14 21:46:02 -0300

    Filtrando de maneiras mais robusta a busca do usuário

--- cars/views.py
+++ cars/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from cars.models import Car
 
 def cars_view(request):
-    cars = Car.objects.all()
+    cars = Car.objects.all().order_by('model')
+    search = request.GET.get('search')
+
+    if search: # Verifica se é não nula
+        cars = cars.filter(model__contains=search)
 
     return render(request, 
                   'cars.html',

a9fcafa1e793409976b524702ea8c9dce4a3a568
Christian Machado <realchris.machado@gmail.com>
2025-10-20 21:55:26 -0300

    Criando classe o tipo UpdateView para atualizar registros dos carros

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,6 @@
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 
 
 class CarsListView(ListView):
@@ -24,6 +24,14 @@ class NewCarCreateView(CreateView):
     template_name = 'new_car.html'
     success_url = '/cars/'
 
+
 class CarDetailView(DetailView):
     model = Car
     template_name = 'car_detail.html'
+
+
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'car_update.html'
+    success_url = '/cars/'

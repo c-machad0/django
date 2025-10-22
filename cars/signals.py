97dc578c63fa4c4ec73f78f6d391d6d0617ae5dc
Christian Machado <realchris.machado@gmail.com>
2025-10-21 21:23:27 -0300

    Criando bio gerada por inteligencia artifical e relacionando models.py com signals.py

--- cars/signals.py
+++ cars/signals.py
@@ -1,7 +1,8 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import pre_save, post_save, post_delete
 from django.db.models import Sum
 from django.dispatch import receiver
 from cars.models import Car, CarInventory
+
 
 
 def car_inventory_update():
@@ -15,6 +16,12 @@ def car_inventory_update():
         cars_value=cars_value
     )
 
+@receiver(pre_save, sender=Car)
+def car_pre_save(sender, instance, **kwargs):
+    if not instance.bio:
+        instance.bio = 'Bio gerada automaticamente!'
+
+
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
     car_inventory_update()
@@ -23,3 +30,13 @@ def car_post_save(sender, instance, **kwargs):
 @receiver(post_delete, sender=Car)
 def car_post_delete(sender, instance, **kwargs):
     car_inventory_update()
+
+"""
+@receiver(pre_save, sender=Car)
+def car_pre_save(sender, instance, **kwargs):
+    if not instance.bio:
+        ai_bio = get_car_ai_bio(
+            instance.model, instance.brand, instance.model_year
+        )
+        instance.bio = ai_bio
+"""
